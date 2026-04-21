fc785c0f2b89d7007c902184e36b94b5f8a55d7c
AnnaKravchenk0 <a.kravchenko.pn@ucu.edu.ua>
2026-04-21 14:17:32 +0300

    3rd problem passed? did it using recursive delete

---
+++ 03_Delete_Node_in_a_BST/solution.py
@@ -0,0 +1,42 @@
+"""Definition for a binary tree node."""
+# class TreeNode:
+#     '''
+#     Docstring for TreeNode
+#     '''
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    '''
+    Docstring for Solution
+    '''
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        '''
+        Docstring for deleteNode
+        '''
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+
+        else:
+            if not root.left:
+                return root.right
+            if not root.right:
+                return root.left
+
+
+            cur = root.right
+            while cur.left:
+                cur = cur.left
+
+            root.val = cur.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
